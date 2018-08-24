430fb8cfc8f6a8a882b4f8a4fd8168d9a711df55
Art Goldhammer <art.goldhammer@gmail.com>
2018-08-24 14:23:56 -0400

    refactored

--- essutils/tests/test_utils.py
+++ essutils/tests/test_utils.py
@@ -1,5 +1,13 @@
 from essutils import utils
+import pandas as pd
 
 def test_get_immigration_data():
-    imm = utils.get_immigration_data()
+    imm = utils.get_data(utils.IMMDATA)
     assert "dweight" in imm.columns
+
+
+def test_weighted_value_counts():
+    testdata = {'x1': [10, 20, 25], 'x2': [1, 2, 7]}
+    testdf = pd.DataFrame(testdata)
+    s = utils.weighted_value_counts(testdf, normalize=True)
+    assert(s[25] == 0.7)
--- essutils/utils.py
+++ essutils/utils.py
@@ -1,11 +1,69 @@
 import pandas as pd
+# import numpy as np
+# import matplotlib
+import matplotlib.pyplot as plt
 
-def get_immigration_data():
+IMMDATA = "~/Prog/EurSocSur/data/immdata.csv"
+
+
+def get_data(filename):
     """
     Load ESS immigration data
-    :param none:
+    :param filename: name of file to load, str
     :returns: dataframe
     """
 
-    imm = pd.read_csv("~/Prog/EurSocSur/data/immdata.csv")
-    return imm
+    df = pd.read_csv(filename)
+    return df
+
+
+def weighted_value_counts(x, normalize=False):
+    """
+    weight value counts in first column by weights in second column of x
+    :x: dataframe, values in first column, weights in second
+    :normalize: normalize the counts (sum 1), default False
+    :return: weighted value counts, indexed in descending order
+    :rtype: series
+    """
+
+    c0 = x.columns[0]
+    c1 = x.columns[1]
+    xtmp = x[[c0, c1]].groupby(c0).agg({c1: 'sum'}).sort_values(c1,
+                                                                ascending=False)
+    s = pd.Series(index=xtmp.index, data=xtmp[c1], name=c0)
+    if normalize:
+        s = s / x[c1].sum()
+    return s
+
+
+def barplot(df, var, countries=None):
+    """
+    Make normalized weighted barplot of var
+    :df: dataframe in which var occurs
+    :var: var to plot
+    :countries: list of countries to plot, None (default) means plot all
+    :return: none
+    """
+
+    vars = ['acetalv', 'eimpcnt', 'gvrfgap', 'imbleco', 'imdetbs',
+            'imdetmr', 'imtcjob', 'imwbcrm', 'lwdscwp', 'noimbro',
+            'pplstrd', 'qfimchr', 'qfimcmt', 'qfimedu', 'qfimlng',
+            'qfimwht', 'qfimwsk']
+    assert(var in vars)
+    if not countries:
+        countries = df.cntry.unique()
+    for c in countries:
+        data = df[(df.cntry == c)]
+        fig, axes = plt.subplots(1, 2, sharex=True, sharey=True)
+        for round, ax_num in [(1, 0), (7, 1)]:
+            rdata = data[data.essround == round]
+            qout = weighted_value_counts(rdata[[var, "pspwght"]],
+                                         normalize=True)
+            qout = qout.sort_index()
+            ax = axes[ax_num]
+            if not qout.empty:
+                ax.set_title(f"{var} \n for country {c} round {round}")
+                qout.plot(ax=ax, kind="bar")
+            else:
+                ax.set_title(f"No data for {var}\n country {c} round {round}")
+        plt.show()
